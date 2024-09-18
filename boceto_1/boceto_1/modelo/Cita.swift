5e4a4548c72ed3a1b746ff058b73aab2594b28f5
alumno <alumno@PC-V202-19.uacj.mx>
2024-09-18 10:36:19 -0600

    usar replit plis

---
+++ boceto_1/boceto_1/modelo/Cita.swift
@@ -0,0 +1,18 @@
+//
+//  Cita.swift
+//  boceto_1
+//
+//  Created by alumno on 9/18/24.
+//
+
+import Foundation
+
+struct Cita{
+    var nombre: String
+    var texto: String
+    
+    init(quien: String, que: String) {
+        nombre = quien
+        texto = que
+    }
+}
