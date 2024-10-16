bd4e3bfe232eb0abb97a9004fbefbc4220a50321
alumno <alumno@PC-V202-19.uacj.mx>
2024-10-16 10:39:41 -0600

    vision

--- boceto_2_CellView/boceto_2_CellView/controladores_de_pantalla/estilos/mod_navegador_principal.swift
+++ boceto_2_CellView/boceto_2_CellView/controladores_de_pantalla/estilos/mod_navegador_principal.swift
@@ -5,4 +5,18 @@
 //  Created by alumno on 10/16/24.
 //
 
-import Foundation
+import UIKit
+
+class mod_navegador_principal: UINavigationController {
+    override func viewDidLoad() {
+        // self.navigationBar.isTranslucent = true
+        // self.navigationBar.backgroundColor = .black
+        //self.hidesBarsOnSwipe = true
+        activar_navigation_bar(actviar: false)
+    }
+    
+    func activar_navigation_bar(actviar: Bool){
+        self.navigationBar.isHidden = !actviar
+    }
+
+}
